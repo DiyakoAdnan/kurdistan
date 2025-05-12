32b415cf4ede6c17dc66d1ea8a4e16735ee72961
Smoking Habits <mhamad.07002734@univsul.edu.iq>
2025-05-13 00:38:02 +0300

    Add homepage route

--- div-main/pages/urls.py
+++ div-main/pages/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from pages import views
 
 urlpatterns = [
-   path("", views.home, name="home"),  
-    path("about/", views.about, name="about"),
-    path("sulaymaniyah/", views.sulaymaniyah, name="sulaymaniyah"),
-    path("hawler/", views.hawler, name="hawler"),
- 
-    path("contact/", views.contact, name="contact"),
+    path('', views.index, name='index'),  # This is the fix
+    path('about/', views.about, name='about'),
+    path('sulaymaniyah/', views.sulaymaniyah, name='sulaymaniyah'),
+    path('hawler/', views.hawler, name='hawler'),
+    path('dohuk/', views.dohuk, name='Duhok'),
+    path('contact/', views.contact, name='contact'),
 ]
